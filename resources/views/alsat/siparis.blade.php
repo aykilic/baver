24eb38cced4b3584eb8dddb670cd1270a33e61f6
yuhasel <aykilicibrahim@gmail.com>
2018-05-14 05:32:37 +0300

    yeni

--- resources/views/alsat/siparis.blade.php
+++ resources/views/alsat/siparis.blade.php
@@ -256,28 +256,28 @@
 
                                                 <div style="width:100%;height:90px">
                                                     {{--<div class="row" style="float:right">--}}
-                                                    <label class="col-md-2" style="font-size: 14px;margin-left:55%;margin-top:7px;font-family: monospace, monospace">Ara Toplam:</label>
+                                                    <label class="col-md-2" style="font-size: 14px;margin-left:55.5%;margin-top:7px;font-family: monospace, monospace">Ara Toplam:</label>
                                                     {{--//  <input for="toplam" id="toplam">--}}
-                                                    <div class="col-md-3" style="font-size: 14px">
-                                                        <input id="toplamm" disabled="disabled" class="form-control" style="font-family: monospace, monospace;padding-right:55px" value="0,00" ><span id="t2" style="padding-right:10px;text-align:right" class=" form-control-feedback right ico" >TL</span>
+                                                    <div class="col-md-3" style="font-size: 14px;width:25.5%">
+                                                        <input id="toplamm" disabled="disabled" class="form-control" style="font-family: monospace, monospace;padding-right:55px" value="0,00" ><span id="t2" style="padding-right:45px;text-align:right" class=" form-control-feedback right ico" >TL</span>
                                                     </div>
                                                     {{--</div>--}}
                                                 </div>
                                                 <div style="width:100%;height:90px">
                                                     {{--<div class="row" style="float:right">--}}
-                                                    <label class="col-md-2" style="font-size: 14px;margin-left:55%;margin-top:7px;font-family: monospace, monospace">KDV </label>
+                                                    <label class="col-md-2" style="font-size: 14px;margin-left:55.5%;margin-top:7px;font-family: monospace, monospace">KDV </label>
                                                     {{--//  <input for="toplam" id="toplam">--}}
-                                                    <div class="col-md-3" style="font-size: 14px">
-                                                        <input id="toplamm" disabled="disabled" class="form-control" style="font-family: monospace, monospace;padding-right:55px" value="0,00" ><span id="t2" style="padding-right:10px;text-align:right" class=" form-control-feedback right ico" >TL</span>
+                                                    <div class="col-md-3" style="font-size: 14px;width:25.5%">
+                                                        <input id="toplamm" disabled="disabled" class="form-control" style="font-family: monospace, monospace;padding-right:55px" value="0,00" ><span id="t2" style="padding-right:45px;text-align:right" class=" form-control-feedback right ico" >TL</span>
                                                     </div>
                                                     {{--</div>--}}
                                                 </div>
                                                 <div style="width:100%;height:90px">
                                                     {{--<div class="row" style="float:right">--}}
-                                                    <label class="col-md-2" style="font-size: 14px;margin-left:55%;margin-top:7px;font-family: monospace, monospace">Toplam:</label>
+                                                    <label class="col-md-2" style="font-size: 14px;margin-left:55.5%;margin-top:7px;font-family: monospace, monospace">Toplam:</label>
                                                     {{--//  <input for="toplam" id="toplam">--}}
-                                                    <div class="col-md-3" style="font-size: 14px">
-                                                        <input id="toplamm" disabled="disabled" class="form-control" style="font-family: monospace, monospace;padding-right:55px" value="0,00" ><span id="t2" style="padding-right:10px;text-align:right" class=" form-control-feedback right ico" >TL</span>
+                                                    <div class="col-md-3" style="font-size: 14px;width:25.5%">
+                                                        <input id="toplamm" disabled="disabled" class="form-control" style="font-family: monospace, monospace;padding-right:55px" value="0,00" ><span id="t2" style="padding-right:45px;text-align:right" class=" form-control-feedback right ico" >TL</span>
                                                     </div>
                                                     {{--</div>--}}
                                                 </div>
